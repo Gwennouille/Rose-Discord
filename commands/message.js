3ede396fc5207da4c76831740df68a543c68f596
Gwennouille <63498523+Gwennouille@users.noreply.github.com>
2020-04-12 00:21:48 +0200

    Update message.js

--- commands/message.js
+++ commands/message.js
@@ -1,12 +1,8 @@
 module.exports.run = async (bot, msg, args) => {
   if(args.length < 1) return msg.channel.send("Laissez moi")
-  var message = '';
-  for(let i = 0; i < args.length; i++){
-    message += args [i] + ' ';
-  }
-
+  var message = args.join(' ');
   msg.channel.send(" " + message)
-};
+}
 
 module.exports.help = {
   name: 'message'
