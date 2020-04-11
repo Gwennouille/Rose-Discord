ad1b642e659f12b49a5e755d5375c98129f0a775
Gwennouille <63498523+Gwennouille@users.noreply.github.com>
2020-04-12 00:21:44 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,13 +3,13 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, msg, args) => {
   let hEmbed = new Discord.MessageEmbed()
   .setColor('ff0000')
-  .setTtile('Vous êtes perdu ?')
+  .setTile('Vous êtes perdu ?')
   .setDescription('Je me dévoile à vous')
   .addField('prefixe:', 'Utiliez le préfixe $ avant chaque commande ^^')
   .addField('Commandes:', '-message: Répéter un message choisi')
   .setFooter("J'espère vous avoir aidé...");
   msg.channel.send(hEmbed);
-};
+}
 
 module.exports.help = {
   name: 'help'
