708d48bdb83dbcbb50fba0f3662ce224ad6345de
Gwennouille <63498523+Gwennouille@users.noreply.github.com>
2020-04-11 23:47:49 +0200

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, msg, args) => {
+  let hEmbed = new Discord.MessageEmbed()
+  .setColor('ff0000')
+  .setTtile('Vous êtes perdu ?')
+  .setDescription('Je me dévoile à vous')
+  .addField('Commandes:', '-message: Répéter un message choisi')
+  .addField('prefixe:', 'Utiliez le préfixe $ avant chaque commande ^^')
+  .setFooter("J'espère vous avoir aidé...");
+  msg.channel.send(hEmbed);
+}
+
+module.exports.help = {
+  name: 'help'
+}
