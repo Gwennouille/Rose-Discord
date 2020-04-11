3c72ee561ec8146d6133fd952248d33423a4343b
Gwennouille <63498523+Gwennouille@users.noreply.github.com>
2020-04-11 23:26:36 +0200

    Move message command to commands folder

--- index.js
+++ index.js
@@ -16,14 +16,6 @@ bot.on('message', async (msg) => {
     if(cmd == 'ping'){
       msg.channel.send("pong xD")
     }
-    if(cmd == 'message'){
-      if(args.length < 1) return msg.channel.send("Pas assez d'arguments")
-      var message = ""
-      for(let i = 0; i < args.length; i++){
-        message += args[i] + " "
-      }
-      msg.channel.send(" " + message)
-    }
   }
 });
 
