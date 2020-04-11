8c73ad0db6bd93d7da20f3fb4881f7d2eba9a6d2
Gwennouille <63498523+Gwennouille@users.noreply.github.com>
2020-04-12 00:00:35 +0200

    Update message.js

--- commands/message.js
+++ commands/message.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, msg, args) => {
   if(args.length < 1) return msg.channel.send("Pas assez d'arguments")
-  var message = args.join('');
+  var message = args.join(' ');
   msg.channel.send(" " + message)
 }
 
