fd702afecc5af5de6992d8fa7bea2436bff0b6cf
Gwennouille <63498523+Gwennouille@users.noreply.github.com>
2020-04-12 01:58:45 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -12,7 +12,7 @@ const commandFactory = (command) => {
 };
 
 const getCommand = (name) => {
-  return commands.find((command) => command.name === name);
+  return commands.find((command) => command.name === name));
 };
 
 const getCommandList = () => {
@@ -27,7 +27,7 @@ fs.readdir('./commands/', (err, files) => {
   commandFiles.forEach((commandFiles) => {
     const command = require(`./commands/${commandFiles}`);
 
-  commands.push(commandFactory(command));
+  commands.push(commandFactory(command);
   });
 });
 
