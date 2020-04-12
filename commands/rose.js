0bb2974b8d5d9b321c373347d8a5b6a6da83eff3
Gwennouille <63498523+Gwennouille@users.noreply.github.com>
2020-04-12 05:44:46 +0200

    Create rose.js

---
+++ commands/rose.js
@@ -0,0 +1,12 @@
+module.exports.run = async (bot, msg, args) => {
+  if(args.length < 1) return msg.channel.send("Laissez moi")
+  var message= ""
+  for(let i = 0; i < args.length; i++){
+    message += argd[i] + " "
+  }
+  msg.channel.send(" " + message)
+}
+
+module.exports.help = {
+  name: 'rose'
+}
