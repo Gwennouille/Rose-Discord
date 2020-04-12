f0e20c461e747fd6ccf089995331133ed17f1c7f
Gwennouille <63498523+Gwennouille@users.noreply.github.com>
2020-04-12 05:12:38 +0200

    Delete message.js

--- commands/message.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (bot, msg, args) => {
-  if(args.length < 1) return msg.channel.send("Laissez moi")
-  var message = args.join(' ');
-  msg.channel.send(" " + message)
-}
-
-module.exports.help = {
-  name: 'message'
-}
