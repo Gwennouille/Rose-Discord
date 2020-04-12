8cdc3e48222dd9697c52e146737046c8341fef31
Gwennouille <63498523+Gwennouille@users.noreply.github.com>
2020-04-12 06:02:05 +0200

    Update rose.js

--- commands/rose.js
+++ commands/rose.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, msg, args) => {
   if(args.length < 1) return msg.channel.send("Laissez moi")
   var message= ""
-  for(let i = 0; i < args.length; i++){
+  for(let i = 0; i < args.length; i++) {
     message += argd[i] + " "
   }
   msg.channel.send(" " + message)
